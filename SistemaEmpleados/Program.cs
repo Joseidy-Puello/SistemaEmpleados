dfabff5f90a7af1fdd7106f7036b69bdb72706b8
joseidy puello ciprian <joseidyp10@gmail.com>
2026-07-07 13:57:22 -0400

    me quede en la parte dos agregando las rutas blazor, cambie los empleados

--- SistemaEmpleados/Program.cs
+++ SistemaEmpleados/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();
+builder.Services.AddServerSideBlazor(); // ? agregar esta línea
 
 var app = builder.Build();
 
@@ -24,4 +26,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapBlazorHub(); // ? agregar esta línea
+app.MapFallbackToPage("/_Host"); // ? agregar esta línea
 app.Run();
