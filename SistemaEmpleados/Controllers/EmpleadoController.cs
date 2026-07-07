f48ee0c40325100ec9a3c66422f4b588662f5c3a
joseidy puello ciprian <joseidyp10@gmail.com>
2026-07-07 13:47:27 -0400

    termine la parte1, Razor

--- SistemaEmpleados/Controllers/EmpleadoController.cs
+++ SistemaEmpleados/Controllers/EmpleadoController.cs
@@ -11,10 +11,10 @@ public class EmpleadoController : Controller
         // Lista hardcodeada — sin base de datos 
         var lista = new List<Empleado>
         {
-            new Empleado { Id=1, Nombre="María García", Departamento="RRHH",Salario=35000m, Activo=true  },
-            new Empleado { Id=2, Nombre="Juan Pérez", Departamento="Tecnología",  Salario=42000m, Activo=true  },
-            new Empleado { Id=3, Nombre="Rosa Martínez",  Departamento="Finanzas", Salario=38000m, Activo=true  },
-            new Empleado { Id=4, Nombre="Pedro Gómez", Departamento="Operaciones", Salario=28000m, Activo=false },
+            new Empleado { Id=1, Nombre="Neiry Rachel", Departamento="RRHH",Salario=350000m, Activo=true  },
+            new Empleado { Id=2, Nombre="Jeiferson David", Departamento="Tecnología",  Salario=4200m, Activo=true  },
+            new Empleado { Id=3, Nombre="Dubenny Areche",  Departamento="Finanzas", Salario=7500m, Activo=true  },
+            new Empleado { Id=4, Nombre="Ronaldiño Reyes", Departamento="Operaciones", Salario=280000m, Activo=false },
         };
 
         return View(lista);  // envía la lista completa a la View
